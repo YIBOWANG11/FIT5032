98d3d6fa96eb8b831b76302d37d7f31aa4be1362
lenovo <lenovo@LENOVO-PC>
2021-08-09 13:51:09 +1000

    添加项目文件。

---
+++ fit5032week3/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using fit5032week3.HelloWorld;
+using System.Web.Mvc;
+using fit5032week3.Execise;
+
+namespace fit5032week3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+            Hello hello = new Hello();
+            ViewBag.Message = hello.GetHello();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            ExampleDictionary ed = new ExampleDictionary();
+
+            ed.Example();
+
+            return View();
+        }
+    }
+}

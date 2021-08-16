8934446038418d617981cd83116a8be15260019c
lenovo <lenovo@LENOVO-PC>
2021-08-16 13:33:09 +1000

    Add project files.

---
+++ fit5032week4  HD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fit5032week4__HD.MvcApplication" Language="C#" %>
---
+++ fit5032week4  HD/Models/Db.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fit5032week4__HD.Models
+{
+    public class Db
+    {
+    }
+}

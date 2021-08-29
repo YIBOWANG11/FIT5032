af9ef8966b6ae4bb12786eb21b19d36b7abe74c6
YIBO-Wang11 <ywan0508@student.monash.edu>
2021-08-29 22:58:40 +1000

    添加项目文件。

---
+++ week6_c/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="week6_c.MvcApplication" Language="C#" %>
---
+++ week6_c/Models/SampleFormViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace week6_c.Models
+{
+    public class SampleFormViewModels
+    {
+    }
+
+    public class FormOneViewModel {
+        [Required]
+        [StringLength(5)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}
